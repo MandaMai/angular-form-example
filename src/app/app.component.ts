9fffc5dd5dc3957302272f300c575db973f3334e
MandaMai <a.maifeld@hotmail.com>
2017-09-25 11:11:43 -0700

    with validation messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import {NgForm} from '@angular/forms'
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,68 @@ import {NgForm} from '@angular/forms'
 export class AppComponent {
   title = 'app';
 
+  // Allows the parent to query child items
+  sampleForm: NgForm;
+  @ViewChild('sampleForm') currentForm: NgForm;
+  // current form is a temporary form or copy of sampleForm
+
   model: object = {
     first_name: "Morty",
     last_name: "Smith"
   }
-
 
   onSubmit(data: NgForm) {
     console.log(data.value);
     console.log(this.model);
   }
 
-  
+  ngAfterViewChecked() {
+    this.formChanged();
+  }
+
+  formChanged() {
+    //if the form didn't change then do nothing
+    if (this.currentForm === this.sampleForm) { return; }
+    //set the form to the current form for comparison
+    this.sampleForm = this.currentForm;
+    //subscribe to form changes and send the changes to the onValueChanged method
+    this.sampleForm.valueChanges
+      .subscribe(() => this.onValueChanged());
+      // .subscribe(data => this.onValueChanged(data));
+  }
+
+  onValueChanged() {
+    let form = this.sampleForm.form;
+
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
+  //start out the errors as an emtpy string
+  formErrors = {
+    'first_name': '',
+    'last_name': ''
+  };
+
+  validationMessages = {
+    'first_name': {
+      'required':      'First name is required.',
+      'minlength':     'Name must be at least 2 characters long.'
+    },
+    'last_name': {
+      'minlength':     'Name must be at least 2 characters long.'
+    }
+  };
+
 }
 
